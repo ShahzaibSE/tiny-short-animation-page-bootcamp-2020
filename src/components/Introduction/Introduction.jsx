a46867a112e2409e2de612c84c6faf17c4968664
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-07-22 00:53:14 +0500

    Setting up index page.

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {Grid} from "@material-ui/core"
+import cx from "classes"
 import useWebAnimations, {fadeIn} from "@wellyshen/use-web-animations"
 // Assets.
 import office_meeting_pic from "./../../images/office-illustration.jpg"
+import styles_from_sheet from "./Introduction.module.scss"
 
 const Introduction = () => {
     const {ref} = useWebAnimations({...fadeIn, 
@@ -11,7 +13,7 @@ const Introduction = () => {
         <Grid container spacing={2}>
             <Grid item xs={4}>
                {/* <StyleRoot>  */}
-                <div ref={ref}>
+                <div className={styles_from_sheet.introduction_container} ref={ref}>
                     <img src={office_meeting_pic} alt="Team Meeting"/>
                 </div>
                 {/* </StyleRoot> */}
